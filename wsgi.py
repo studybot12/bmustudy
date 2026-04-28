395bb2497efa1162f3391d490f2ae9d21f38f7dd
studybot12 <cifkif1@gmail.com>
2026-04-29 00:07:14 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ from app import app
 if __name__ == "__main__":
     from database import db
     db.init()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
